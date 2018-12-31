14bb0788ec100e314436cb5eddbb8ba80a099c14
nikkimegaplaza <45581791+nikkimegaplaza@users.noreply.github.com>
2018-12-30 20:41:00 -0500

    Update exercise1.py

--- lesson 4/exercise1.py
+++ lesson 4/exercise1.py
@@ -13,39 +13,33 @@ Using a single for-loop iterate over the dictionary and print out all of the dic
 values.
 """
 from __future__ import unicode_literals, print_function
+	
 
-net_device = {
-    'ip_addr': '10.10.10.10',
-    'vendor': 'cisco',
-    'username': 'admin',
-    'password': 'password'
-}
-
+my_dict = {
+	"ip_addr": "9.9.9.9",
+	"vendor" : "cisco",
+	"username" : "admin",
+	"password" : "pass"
+	}
+print()	
+print(my_dict["ip_addr"])
 print()
-print(net_device['ip_addr'])
-print()
 
-if net_device['vendor'].lower() == 'cisco':
-    net_device['platform'] = 'ios'
-elif net_device['vendor'].lower() == 'juniper':
-    net_device['platform'] = 'junos'
+if my_dict["vendor"].lower() == "cisco" :
+	my_dict["platform"] = "ios"
+elif my_dict["vendor"].lower() == "juniper" :
+	my_dict["platform"] = "junos:"
+	
 
 bgp_fields = {
-    'bgp_as': 42,
-    'peer_as': 100,
-    'peer_ip': '172.16.31.100',
+	'bgp_as' : 42,
+	'peer_as' : 100,
+	'peer_ip' : '1.1.1.1'
 }
 
-net_device.update(bgp_fields)
+my_dict.update(bgp_fields)
 
-print('-' * 80)
-for key in net_device:
-    print("{:>15}".format(key))
-print('-' * 80)
-print()
-
-print('-' * 80)
-for key, value in net_device.items():
-    print("{key:>15} ---> {value:>15}".format(key=key, value=value))
-print('-' * 80)
-print()
+print("-" * 50)
+for x,y in my_dict.items():
+	#print("{key"":>15} {value}".format(key=x,value=y))
+	print("{key:>15} ---> {value}".format(key=x, value=y))
